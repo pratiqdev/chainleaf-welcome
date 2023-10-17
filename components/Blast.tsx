715edfc97dc6757d62318fe6fc4d72d9498c92d4
pratiqdev <pratiqdev@gmail.com>
2023-10-17 18:50:47 -0400

    switch to axiosInstance

--- components/Blast.tsx
+++ components/Blast.tsx
@@ -22,21 +22,27 @@ const BlastForm = (props: BlastProps) => {
     const ctx = useAuth()
 
     const send = async () => {
-        if (!email || !emailRegexp.test(email)) {
-            console.log('Invalid email:', email)
-            setError('Invalid email!')
-            return
-        }
-        console.log('sending email to ' + email)
-        setError('Sending email...')
-         const { data } = await axiosInstance.post('/chainleaflabs-usersubscriptions/usersubscriptions/chainleaflabs/subscribeforupdates', JSON.stringify({
-            "user_subscription_details": {
-                email: email,
-                origin: 'chainleaf welcome banner email subscriber',
-                envData,
-                userTime,
+        try{
+            
+            if (!email || !emailRegexp.test(email)) {
+                console.log('Invalid email:', email)
+                setError('Invalid email!')
+                return
             }
-        }))
+            console.log('sending email to ' + email)
+            setError('Sending email...')
+            const { data } = await axiosInstance.post('/chainleaflabs-usersubscriptions/usersubscriptions/chainleaflabs/subscribeforupdates', JSON.stringify({
+                "user_subscription_details": {
+                    email: email,
+                    origin: 'chainleaf welcome banner email subscriber',
+                    envData,
+                    userTime,
+                }
+            }))
+
+            if('UserSubscription' in data){
+                router.push(`/join/subscribed?email=${email}`)
+            }
 
         // fetch(url, {
         //     method: 'POST',
@@ -59,6 +65,10 @@ const BlastForm = (props: BlastProps) => {
         //         }
         //     })
         //     .catch((error) => console.error('Error:', error));
+        }catch(err){
+            console.log('Email service error:', err)
+            setError('Email service error...')
+        }
     }
 
 
@@ -67,7 +77,7 @@ const BlastForm = (props: BlastProps) => {
         <>
             <div className="border border-primary-5 text-primary-5 mb-3 rounded h-min overflow-hidden flex items-center max-w-[32rem] mx-6 md:mx-0">
                 <input placeholder={error || props.placeholder} type="text" value={error || email} className="p-2 md:p-4 flex-1 bg-transparent placeholder-primary-5" onChange={(e) => {setError(''); setEmail(e.target.value)}} />
-                <button onClick={send} className="p-2 md:p-4 hover:bg-primary-5 hover:text-white dark:bg-transparent dark:text-primary-5 dark:hover:text-primary-1 dark:hover:bg-primary-4 duration-200 font-bold text-xl">-&gt;</button>
+                <button onClick={send} className="p-2 md:p-4 hover:bg-primary-5 hover:text-white dark:bg-transparent dark:text-primary-5 dark:hover:text-primary-1 dark:hover:bg-primary-4 duration-200 font-bold text-xl whitespace-nowrap">-&gt;</button>
             </div>
             {/* {error && <small>{error}</small>} */}
         </>
